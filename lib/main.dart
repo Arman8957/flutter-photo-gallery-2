6f6637f1d3562ea6cb6066e11122a5d5b011ee2c
Arman8957 <novicethought0@gmail.com>
2025-05-06 21:01:28 +0600

    screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:photo_gallery_app/add_screen.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
